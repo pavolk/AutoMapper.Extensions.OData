7f28b41a0f00d3298e57bc397ba0c4f2817a563f
Pavol Kurina <pavol.kurina@gmail.com>
2023-04-02 22:25:58 +0200

    added devicecontroller

---
+++ WebAPI.OData.EFCore/Controllers/DeviceController.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using AutoMapper.AspNet.OData;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.OData.EFCore.Controllers
+{
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    [Route("api/devices")]
+    [ApiController]
+    public class DeviceController : ODataController
+    {
+        List<Device> _devices = new List<Device>();
+
+        IMapper _mapper;
+
+        public DeviceController(IMapper mapper) 
+        {
+            _mapper = mapper;
+
+            _devices.Add(new Device() { Id = "1", Modem = new Modem() { Name = "Modem-1" } });
+            _devices.Add(new Device() { Id = "2" });
+
+        }
+
+        // GET: api/<DeviceController>
+        [HttpGet]
+        public async Task<IActionResult> Get(ODataQueryOptions<DeviceDto> options)
+        {
+            return Ok(await _devices.AsQueryable<Device>().GetQueryAsync(_mapper, options,
+                            new QuerySettings { 
+                                ODataSettings = new ODataSettings { HandleNullPropagation = HandleNullPropagationOption.False } }));
+
+            // return Ok(_devices);
+        }
+
+        // GET api/<DeviceController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DeviceController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DeviceController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DeviceController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebAPI.OData.EFCore/Controllers/Model.cs
@@ -0,0 +1,47 @@
+﻿namespace WebAPI.OData.EFCore.Controllers
+{
+    public class Device
+    {
+        public string Id { get; set; }
+
+        public Modem? Modem { get; set; } = null;
+    }
+
+    public class Modem 
+    {
+        public string Name { get; set; } = string.Empty;
+   
+    }
+
+    public class DeviceDto 
+    {
+        public string Id { get; set; }
+
+        public ModemDto? Modem { get; set; } = null;
+    }
+
+    public class ModemDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+    }
+
+
+    public class Mappings : AutoMapper.Profile 
+    {
+        public Mappings() 
+        { 
+            CreateMap<Device, DeviceDto>()
+                .ForAllMembers(o => o.ExplicitExpansion());
+
+            CreateMap<Device, DeviceDto>()
+                .ReverseMap()
+                    .ForAllMembers(o => o.ExplicitExpansion());
+
+            CreateMap<Modem, ModemDto>()
+                .ForAllMembers(o => o.ExplicitExpansion());
+
+        }
+    
+    }
+}
