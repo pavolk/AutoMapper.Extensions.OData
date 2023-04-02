d2e3a60e6c9e7a3ab1ef2081bccc6dab2f1e812d
Pavol Kurina <pavol.kurina@gmail.com>
2023-04-02 22:23:57 +0200

    seeding db in context

--- DAL.EFCore/MyDbContext.cs
+++ DAL.EFCore/MyDbContext.cs
@@ -1,16 +1,62 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DAL.EFCore
 {
     public class MyDbContext : DbContext
     {
+        public static void SeedDatabase(MyDbContext context)
+        {
+            if (context.City.Any())
+                return;
+
+            context.City.Add(new TCity { Name = "London" });
+            context.City.Add(new TCity { Name = "Leeds" });
+            context.SaveChanges();
+
+            List<TCity> cities = context.City.ToList();
+            context.Builder.Add(new TBuilder { Name = "Sam", CityId = cities.First(b => b.Name == "London").Id });
+            context.Builder.Add(new TBuilder { Name = "John", CityId = cities.First(b => b.Name == "London").Id });
+            context.Builder.Add(new TBuilder { Name = "Mark", CityId = cities.First(b => b.Name == "Leeds").Id });
+            context.SaveChanges();
+
+            List<TBuilder> builders = context.Builder.ToList();
+            context.MandatorSet.Add(new TMandator {
+                Identity = Guid.NewGuid(),
+                Name = "One",
+                CreatedDate = new DateTime(2012, 12, 12),
+                Buildings = new List<TBuilding>
+                {
+                    new TBuilding { Identity =  Guid.NewGuid(), LongName = "One L1", BuilderId = builders.First(b => b.Name == "Sam").Id },
+                    new TBuilding { Identity =  Guid.NewGuid(), LongName = "One L2", BuilderId = builders.First(b => b.Name == "Sam").Id  }
+                }
+            });
+            context.MandatorSet.Add(new TMandator {
+                Identity = Guid.NewGuid(),
+                Name = "Two",
+                CreatedDate = new DateTime(2012, 12, 12),
+                Buildings = new List<TBuilding>
+                {
+                    new TBuilding { Identity =  Guid.NewGuid(), LongName = "Two L1", BuilderId = builders.First(b => b.Name == "John").Id  },
+                    new TBuilding { Identity =  Guid.NewGuid(), LongName = "Two L2", BuilderId = builders.First(b => b.Name == "Mark").Id  },
+                    new TBuilding { Identity =  Guid.NewGuid(), LongName = "Two L3", BuilderId = builders.First(b => b.Name == "Mark").Id  }
+                }
+            });
+            context.SaveChanges();
+        }
+
+
         public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
         {
             //this.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
             this.ChangeTracker.AutoDetectChangesEnabled = false;
+
+            this.Database.EnsureCreated();
+            SeedDatabase(this);
         }
 
         public DbSet<TMandator> MandatorSet { get; set; }
